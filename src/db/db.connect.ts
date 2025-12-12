d7ae758e2e961d31d02e2b4441cfb10711f1c9cc
Amresh-01 <amreshchaurasiya373@gmail.com>
2025-12-12 16:55:26 +0530

    Add drizzle orm

--- src/db/db.connect.ts
+++ src/db/db.connect.ts
@@ -1,17 +1,24 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const connectDb = async() => {
+// try {
+//   const Mongo_Url = process.env.MONGO_URI as string;
+//   await mongoose.connect(Mongo_Url);
+//   console.log("MongoDb is connected....");
+// } catch (error) {
+//   console.error("Error in connecting to Database....", error);
+//   process.exit(1);
+// }
+// }
+
+// export default connectDb;
+
+import { drizzle } from 'drizzle-orm/neon-http';
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const connectDb = async() => {
-try {
-  const Mongo_Url = process.env.MONGO_URI as string;
-  await mongoose.connect(Mongo_Url);
-  console.log("MongoDb is connected....");
-} catch (error) {
-  console.error("Error in connecting to Database....", error);
-  process.exit(1);
-}
-}
-
-export default connectDb;
+const db = drizzle(process.env.DATABASE_URL as string);
